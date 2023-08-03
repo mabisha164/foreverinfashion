b324c7024d4ba64765b80e99245fcc3ad0c6b9c7
mabisha164 <mabisha164@gmail.com>
2023-08-03 13:37:55 +0545

    Description changes

--- src/Components/frontend/Pages/Description.jsx
+++ src/Components/frontend/Pages/Description.jsx
@@ -43,51 +43,47 @@ const Description = ({ addToCart }) => {
 
   const { name, img, description, price } = item;
   return (
-    <div className="">
+    <div className="ml-36">
       <h1>
         <b className="text-5xl italic text-orange-500 flex justify-center items-center mt-4 underline-offset-2 font-cursive">
           Item Description
         </b>
       </h1>
-      <div className="flex ">
+      <div className="flex mt-10 bg-blue-100 w-[80%] ml-16 rounded-2xl shadow-2xl">
         <div className="flex ">
           <br />
-          <div className=" my-4  border-4  border-white rounded-2xl shadow-2xl w-[450px] ml-36">
-            <img
-              className="w-[450px] mr-10  rounded-xl  "
-              src={img}
-              alt={name}
-            />
+          <div className=" my-2   border-2  border-white rounded-2xl shadow-2xl w-[450px] ml-16">
+            <img className="w-[450px]  rounded-xl  " src={img} alt={name} />
           </div>
         </div>
-        {/* <div className="">
-          <h1 className=" absolute bottom-0 ml-10 rounded-xl shadow-2xl right-1/2 left-1/2 text-3xl bg-green-200 h-10 w-36 text-center">
-            {price}
-          </h1>
-        </div> */}
-        <div className="ml-10">
-          <h2 className=" text-3xl mt-6 ml-5 font-cursive underline hover:underline-offset-4">
+        <div className="">
+          <h2 className=" text-4xl mt-20  font-cursive underline hover:underline-offset-4 ml-16">
             {name}
           </h2>
           <br />
-          <p className="  text-xl   font-italic  leading-relaxed">
+          <p className="  text-xl   font-italic  leading-relaxed mr-16 ml-16">
             {description}
           </p>
-        </div>
-      </div>
-      <div>
-        <button
-          onClick={(e) => {
-            e.preventDefault();
-
-            addToCart({ ...item, selectedSize });
-          }}
-        >
-          <div className="relative top-0 flex justify-center items-center text-center ml-[650px] text-white hover:bg-orange-300 hover:text-white bg-red-500 w-[200px] h-[50px] text-xl  shadow-2xl rounded-lg font-cursive">
-            <BsPlus size={20} className="h-12 w-20" />
-            Add to cart
+          <div className="flex">
+            <h1 className=" absolute mt-6 mr-16 arounded-xl  ml-16 text-3xl font-cursive text-orange-600 h-10 w-36 text-center rounded-2xl shadow-2xl ">
+              {price}
+            </h1>
           </div>
-        </button>
+          <div>
+            <button
+              onClick={(e) => {
+                e.preventDefault();
+
+                addToCart({ ...item, selectedSize });
+              }}
+            >
+              <div className="relative top-0 flex justify-center items-center text-center mt-24 text-white hover:bg-orange-300 hover:text-white bg-red-500 w-[200px] h-[50px] text-xl  shadow-2xl rounded-lg font-cursive ml-16">
+                <BsPlus size={20} className="h-12 w-20" />
+                Add to cart
+              </div>
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
